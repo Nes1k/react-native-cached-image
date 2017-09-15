ce306c79ee24aa5b973a1e39e85a6bff16515c4b
Dominik Biel <nes1kd@gmail.com>
2017-09-15 09:52:19 +0200

    use es6 class

--- ImageCacheProvider.js
+++ ImageCacheProvider.js
@@ -1,19 +1,19 @@
-'use strict';
+
 
 const _ = require('lodash');
 
 const RNFetchBlob = require('react-native-fetch-blob').default;
 
 const {
-    fs
+    fs,
 } = RNFetchBlob;
 
 const LOCATION = {
-    CACHE: fs.dirs.CacheDir + '/imagesCacheDir',
-    BUNDLE: fs.dirs.MainBundleDir + '/imagesCacheDir'
+  CACHE: `${fs.dirs.CacheDir}/imagesCacheDir`,
+  BUNDLE: `${fs.dirs.MainBundleDir}/imagesCacheDir`,
 };
 
-const SHA1 = require("crypto-js/sha1");
+const SHA1 = require('crypto-js/sha1');
 const URL = require('url-parse');
 
 const defaultHeaders = {};
@@ -21,14 +21,14 @@ const defaultImageTypes = ['png', 'jpeg', 'jpg', 'gif', 'bmp', 'tiff', 'tif'];
 const defaultResolveHeaders = _.constant(defaultHeaders);
 
 const defaultOptions = {
-    useQueryParamsInCacheKey: false,
-    cacheLocation: LOCATION.CACHE
+  useQueryParamsInCacheKey: false,
+  cacheLocation: LOCATION.CACHE,
 };
 
 const activeDownloads = {};
 
 function serializeObjectKeys(obj) {
-    return _(obj)
+  return _(obj)
         .toPairs()
         .sortBy(a => a[0])
         .map(a => a[1])
@@ -36,55 +36,55 @@ function serializeObjectKeys(obj) {
 }
 
 function getQueryForCacheKey(url, useQueryParamsInCacheKey) {
-    if (_.isArray(useQueryParamsInCacheKey)) {
-        return serializeObjectKeys(_.pick(url.query, useQueryParamsInCacheKey));
-    }
-    if (useQueryParamsInCacheKey) {
-        return serializeObjectKeys(url.query);
-    }
-    return '';
+  if (_.isArray(useQueryParamsInCacheKey)) {
+    return serializeObjectKeys(_.pick(url.query, useQueryParamsInCacheKey));
+  }
+  if (useQueryParamsInCacheKey) {
+    return serializeObjectKeys(url.query);
+  }
+  return '';
 }
 
 function generateCacheKey(url, options) {
-    const parsedUrl = new URL(url, null, true);
+  const parsedUrl = new URL(url, null, true);
 
-    const pathParts = parsedUrl.pathname.split('/');
+  const pathParts = parsedUrl.pathname.split('/');
 
     // last path part is the file name
-    const fileName = pathParts.pop();
-    const filePath = pathParts.join('/');
+  const fileName = pathParts.pop();
+  const filePath = pathParts.join('/');
 
-    const parts = fileName.split('.');
-    const fileType = parts.length > 1 ? _.toLower(parts.pop()) : '';
-    const type = defaultImageTypes.includes(fileType) ? fileType : 'jpg';
+  const parts = fileName.split('.');
+  const fileType = parts.length > 1 ? _.toLower(parts.pop()) : '';
+  const type = defaultImageTypes.includes(fileType) ? fileType : 'jpg';
 
-    const cacheable = filePath + fileName + type + getQueryForCacheKey(parsedUrl, options.useQueryParamsInCacheKey);
-    return SHA1(cacheable) + '.' + type;
+  const cacheable = filePath + fileName + type + getQueryForCacheKey(parsedUrl, options.useQueryParamsInCacheKey);
+  return `${SHA1(cacheable)}.${type}`;
 }
 
 function getBaseDir(cacheLocation) {
-    return cacheLocation || LOCATION.CACHE;
+  return cacheLocation || LOCATION.CACHE;
 }
 
 function getCachePath(url, options) {
-    if (options.cacheGroup) {
-        return options.cacheGroup;
-    }
-    const {
-        host
+  if (options.cacheGroup) {
+    return options.cacheGroup;
+  }
+  const {
+        host,
     } = new URL(url);
-    return host.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  return host.replace(/[^a-z0-9]/gi, '').toLowerCase();
 }
 
 function getCachedImageFilePath(url, options) {
-    const cachePath = getCachePath(url, options);
-    const cacheKey = generateCacheKey(url, options);
+  const cachePath = getCachePath(url, options);
+  const cacheKey = generateCacheKey(url, options);
 
-    return `${getBaseDir(options.cacheLocation)}/${cachePath}/${cacheKey}`;
+  return `${getBaseDir(options.cacheLocation)}/${cachePath}/${cacheKey}`;
 }
 
 function deleteFile(filePath) {
-    return fs.stat(filePath)
+  return fs.stat(filePath)
         .then(res => res && res.type === 'file')
         .then(exists => exists && fs.unlink(filePath))
         .catch((err) => {
@@ -93,27 +93,27 @@ function deleteFile(filePath) {
 }
 
 function getDirPath(filePath) {
-    return _.initial(filePath.split('/')).join('/');
+  return _.initial(filePath.split('/')).join('/');
 }
 
 function ensurePath(dirPath) {
-    return fs.isDir(dirPath)
-        .then(isDir => {
-            if (!isDir) {
-                return fs.mkdir(dirPath)
-                    .then(() => fs.exists(dirPath).then(exists => {
+  return fs.isDir(dirPath)
+        .then((isDir) => {
+          if (!isDir) {
+            return fs.mkdir(dirPath)
+                    .then(() => fs.exists(dirPath).then((exists) => {
                         // Check if dir has indeed been created because
                         // there's no exception on incorrect user-defined paths (?)...
-                        if (!exists) throw new Error('Invalid cacheLocation');
+                      if (!exists) throw new Error('Invalid cacheLocation');
                     }))
-            }
+          }
         })
-        .catch(err => {
+        .catch((err) => {
             // swallow folder already exists errors
-            if (err.message.includes('folder already exists')) {
-                return;
-            }
-            throw err;
+          if (err.message.includes('folder already exists')) {
+            return;
+          }
+          throw err;
         });
 }
 
@@ -129,81 +129,75 @@ function ensurePath(dirPath) {
  */
 function downloadImage(fromUrl, toFile, headers = {}) {
     // use toFile as the key as is was created using the cacheKey
-    if (!_.has(activeDownloads, toFile)) {
-        //Using a temporary file, if the download is accidentally interrupted, it will not produce a disabled file
-        const tmpFile = toFile + '.tmp';
+  if (!_.has(activeDownloads, toFile)) {
+        // Using a temporary file, if the download is accidentally interrupted, it will not produce a disabled file
+    const tmpFile = `${toFile}.tmp`;
         // create an active download for this file
-        activeDownloads[toFile] = new Promise((resolve, reject) => {
-            RNFetchBlob
-                .config({path: tmpFile})
+    activeDownloads[toFile] = new Promise((resolve, reject) => {
+      RNFetchBlob
+                .config({ path: tmpFile })
                 .fetch('GET', fromUrl, headers)
-                .then(res => {
-                    if (Math.floor(res.respInfo.status / 100) !== 2) {
-                        throw new Error('Failed to successfully download image');
-                    }
-                    //The download is complete and rename the temporary file
-                    return fs.mv(tmpFile, toFile);
+                .then((res) => {
+                  if (Math.floor(res.respInfo.status / 100) !== 2) {
+                    throw new Error('Failed to successfully download image');
+                  }
+                    // The download is complete and rename the temporary file
+                  return fs.mv(tmpFile, toFile);
                 })
                 .then(() => resolve(toFile))
-                .catch(err => {
-                    return deleteFile(tmpFile)
-                        .then(() => reject(err));
-                })
+                .catch(err => deleteFile(tmpFile)
+                        .then(() => reject(err)))
                 .finally(() => {
                     // cleanup
-                    delete activeDownloads[toFile];
+                  delete activeDownloads[toFile];
                 });
-        });
-    }
-    return activeDownloads[toFile];
+    });
+  }
+  return activeDownloads[toFile];
 }
 
 function createPrefetcer(list) {
-    const urls = _.clone(list);
-    return {
-        next() {
-            return urls.shift();
-        }
-    };
+  const urls = _.clone(list);
+  return {
+    next() {
+      return urls.shift();
+    },
+  };
 }
 
 function runPrefetchTask(prefetcher, options) {
-    const url = prefetcher.next();
-    if (!url) {
-        return Promise.resolve();
-    }
+  const url = prefetcher.next();
+  if (!url) {
+    return Promise.resolve();
+  }
     // if url is cacheable - cache it
-    if (isCacheable(url)) {
+  if (isCacheable(url)) {
         // check cache
-        return getCachedImagePath(url, options)
+    return getCachedImagePath(url, options)
         // if not found download
             .catch(() => cacheImage(url, options))
             // allow prefetch task to fail without terminating other prefetch tasks
             .catch(_.noop)
             // then run next task
             .then(() => runPrefetchTask(prefetcher, options));
-    }
+  }
     // else get next
-    return runPrefetchTask(prefetcher, options);
+  return runPrefetchTask(prefetcher, options);
 }
 
 function collectFilesInfo(basePath) {
-    return fs.stat(basePath)
+  return fs.stat(basePath)
         .then((info) => {
-            if (info.type === 'file') {
-                return [info];
-            }
-            return fs.ls(basePath)
-                .then(files => {
-                    const promises = _.map(files, file => {
-                        return collectFilesInfo(`${basePath}/${file}`);
-                    });
-                    return Promise.all(promises);
+          if (info.type === 'file') {
+            return [info];
+          }
+          return fs.ls(basePath)
+                .then((files) => {
+                  const promises = _.map(files, file => collectFilesInfo(`${basePath}/${file}`));
+                  return Promise.all(promises);
                 });
         })
-        .catch(err => {
-            return [];
-        });
+        .catch(err => []);
 }
 
 // API
@@ -215,7 +209,7 @@ function collectFilesInfo(basePath) {
  * @returns {boolean}
  */
 function isCacheable(url) {
-    return _.isString(url) && (_.startsWith(url, 'http://') || _.startsWith(url, 'https://'));
+  return _.isString(url) && (_.startsWith(url, 'http://') || _.startsWith(url, 'https://'));
 }
 
 /**
@@ -225,24 +219,24 @@ function isCacheable(url) {
  * @returns {Promise.<String>}
  */
 function getCachedImagePath(url, options = defaultOptions) {
-    const filePath = getCachedImageFilePath(url, options);
-    return fs.stat(filePath)
-        .then(res => {
-            if (res.type !== 'file') {
+  const filePath = getCachedImageFilePath(url, options);
+  return fs.stat(filePath)
+        .then((res) => {
+          if (res.type !== 'file') {
                 // reject the promise if res is not a file
-                throw new Error('Failed to get image from cache');
-            }
-            if (!res.size) {
+            throw new Error('Failed to get image from cache');
+          }
+          if (!res.size) {
                 // something went wrong with the download, file size is 0, remove it
-                return deleteFile(filePath)
+            return deleteFile(filePath)
                     .then(() => {
-                        throw new Error('Failed to get image from cache');
+                      throw new Error('Failed to get image from cache');
                     });
-            }
-            return filePath;
+          }
+          return filePath;
         })
-        .catch(err => {
-            throw err;
+        .catch((err) => {
+          throw err;
         })
 }
 
@@ -254,9 +248,9 @@ function getCachedImagePath(url, options = defaultOptions) {
  * @returns {Promise.<String>}
  */
 function cacheImage(url, options = defaultOptions, resolveHeaders = defaultResolveHeaders) {
-    const filePath = getCachedImageFilePath(url, options);
-    const dirPath = getDirPath(filePath);
-    return ensurePath(dirPath)
+  const filePath = getCachedImageFilePath(url, options);
+  const dirPath = getDirPath(filePath);
+  return ensurePath(dirPath)
         .then(() => resolveHeaders())
         .then(headers => downloadImage(url, filePath, headers));
 }
@@ -268,8 +262,8 @@ function cacheImage(url, options = defaultOptions, resolveHeaders = defaultResol
  * @returns {Promise}
  */
 function deleteCachedImage(url, options = defaultOptions) {
-    const filePath = getCachedImageFilePath(url, options);
-    return deleteFile(filePath);
+  const filePath = getCachedImageFilePath(url, options);
+  return deleteFile(filePath);
 }
 
 /**
@@ -280,12 +274,12 @@ function deleteCachedImage(url, options = defaultOptions) {
  * @returns {Promise}
  */
 function cacheMultipleImages(urls, options = defaultOptions) {
-    const prefetcher = createPrefetcer(urls);
-    const numberOfWorkers = urls.length;
-    const promises = _.times(numberOfWorkers, () =>
-        runPrefetchTask(prefetcher, options)
+  const prefetcher = createPrefetcer(urls);
+  const numberOfWorkers = urls.length;
+  const promises = _.times(numberOfWorkers, () =>
+        runPrefetchTask(prefetcher, options),
     );
-    return Promise.all(promises);
+  return Promise.all(promises);
 }
 
 /**
@@ -296,9 +290,9 @@ function cacheMultipleImages(urls, options = defaultOptions) {
  * @returns {Promise}
  */
 function deleteMultipleCachedImages(urls, options = defaultOptions) {
-    return _.reduce(urls, (p, url) =>
+  return _.reduce(urls, (p, url) =>
             p.then(() => deleteCachedImage(url, options)),
-        Promise.resolve()
+        Promise.resolve(),
     );
 }
 
@@ -323,7 +317,7 @@ function seedCache(local, url, options = defaultOptions) {
  * @returns {Promise}
  */
 function clearCache(cacheLocation) {
-    return fs.unlink(getBaseDir(cacheLocation))
+  return fs.unlink(getBaseDir(cacheLocation))
         .catch(() => {
             // swallow exceptions if path doesn't exist
         })
@@ -336,28 +330,28 @@ function clearCache(cacheLocation) {
  * @returns {Promise.<{size}>}
  */
 function getCacheInfo(cacheLocation) {
-    return ensurePath(getBaseDir(cacheLocation))
+  return ensurePath(getBaseDir(cacheLocation))
         .then(() => collectFilesInfo(getBaseDir(cacheLocation)))
-        .then(cache => {
-            const files = _.flattenDeep(cache);
-            const size = _.sumBy(files, 'size');
-            return {
-                files,
-                size
-            };
+        .then((cache) => {
+          const files = _.flattenDeep(cache);
+          const size = _.sumBy(files, 'size');
+          return {
+            files,
+            size,
+          };
         });
 }
 
 module.exports = {
-    isCacheable,
-    getCachedImageFilePath,
-    getCachedImagePath,
-    cacheImage,
-    deleteCachedImage,
-    cacheMultipleImages,
-    deleteMultipleCachedImages,
-    clearCache,
-    seedCache,
-    getCacheInfo,
-    LOCATION
+  isCacheable,
+  getCachedImageFilePath,
+  getCachedImagePath,
+  cacheImage,
+  deleteCachedImage,
+  cacheMultipleImages,
+  deleteMultipleCachedImages,
+  clearCache,
+  seedCache,
+  getCacheInfo,
+  LOCATION,
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 module.exports = require('./CachedImage');
 module.exports.ImageCacheProvider = require('./ImageCacheProvider');
